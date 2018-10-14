e4bcf65b046b5ce52ea509abdcab40ac47abd8e6
barbaradecares <barbaradecares@hotmail.com>
2018-10-13 21:34:11 -0500

    anagram detector

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+    attr_accessor :word, :anagram
+
+    def initialize(word)
+    self.word = word
+    end 
+
+    def match(array_of_words)
+        @anagram = []
+
+        array_of_words.each do |word_to_check|
+            
+            letters = word_to_check.split("")
+          
+            if letters.sort == self.word.split("").sort
+                @anagram << word_to_check
+            
+    
+            end
+            #binding.pry
+        end 
+
+    @anagram
+    end 
+end 
